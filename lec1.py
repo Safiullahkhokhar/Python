19cc87fa48ea853672e7447d24c44b6b9bdbdf05
Safiullah khokhar <safiullahkhokhar7@gmail.com>
2023-09-27 05:43:21 -0700

    variables topic added

--- lec1.py
+++ lec1.py
@@ -1,4 +1,16 @@
-print("hello")
+# this is comment using hash symbol #
+
+print("hello") # how to print any messange in python
 print (5)
-print ("hey")
-print("bye")
+print ("hey",7)
+
+a=1 # integer vartiable
+b=2
+print(a+b) #adding 2 variables
+
+print(4,5, sep="~") # "break on center"
+print(6,7,end="00 Added in last\n") # something adding in last 
+
+print("The type of is:",type(a)) # show the type of any variable
+
+
