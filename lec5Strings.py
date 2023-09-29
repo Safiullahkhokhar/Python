a90299db2c38d76e223740136624df0af5f26e44
Safiullah khokhar <safiullahkhokhar7@gmail.com>
2023-09-29 03:45:23 -0700

    string scling

---
+++ lec5Strings.py
@@ -0,0 +1,39 @@
+#single sting line
+
+# apple='He said, "I want to eat an apple".'
+# print(apple)
+
+#multiple srings pirnt
+
+# a="""Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+# access index point of any srting 
+
+# acc="Safi"
+# print(acc [0])
+# print(acc [1])
+# print(acc [2])
+# print(acc [3])
+
+# String slicing
+f1="mango"
+print(f1[0:4])
+
+#-5 -4 -3 -2 -1
+# M  a  n  g  o
+# 0  1  2  3  4
+
+fruit="Mango"
+print(fruit[-3 : -1]) # ng
+print(fruit[-3 : -2]) # n
+print(fruit[ : : 88]) # M
+print(fruit[-3 : -1]) # ng
+print(fruit[ 2 : 4])  # ng
+print(fruit[-4 : -2]) # an
+print(fruit[1 : 3])   # an
+print(fruit[: : -1])  # ognaM
+print(fruit[ : : -2]) # onM
