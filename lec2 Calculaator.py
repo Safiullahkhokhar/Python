8adcb84c77371c820ca953cc9de243697761cb5c
Safiullah khokhar <safiullahkhokhar7@gmail.com>
2023-09-28 22:10:09 -0700

    added new lectures

---
+++ lec2 Calculaator.py
@@ -0,0 +1,14 @@
+# operators
+#Exercise No.1 Calculator
+
+print("Exercise output")
+n1=15
+n2=3
+print("The value of",n1,"+ ",n2,"is =", n1 + n2)
+print("The value of",n1,"- ",n2,"is =",n1 - n2)
+print("The value of",n1,"* ",n2,"is =",n1 * n2)
+print("The value of",n1,"**",n2,"is =",n1 ** n2)
+print("The value of",n1,"% ",n2,"is =",n1 % n2)
+print("The value of",n1,"/ ",n2,"is =",n1 / n2)
+print("The value of",n1,"//",n2,"is =",n1 // n2)
+
