ed1c041618d94fd32bc17c20e58ae56b2ec949fe
Safiullah khokhar <safiullahkhokhar7@gmail.com>
2023-10-02 11:18:25 -0700

    for loop

--- lec9MatchCase.py
+++ lec9MatchCase.py
@@ -10,3 +10,21 @@ match ip:
         print("Not available")
 
 # only match the case numbers otherwise it will excute default case 
+days = "Friday"
+match days:
+    case "Monday":
+        print("Today is Monday!")
+    case "Tuesday":
+        print("Today is Tuesday!")
+    case "Wednesday":
+        print("Today is Wednesday!")
+    case "Thursday":
+        print("Today is Thursday!")
+    case "Friday":
+        print("Today is Friday!")
+    case "Saturaday":
+        print("Today is Saturaday!")
+    case "Sunday":
+        print("Today is Sunday!")
+    case _:
+        print("Wrong")
