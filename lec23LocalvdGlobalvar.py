cc4e8ff99f3c3d26fbb86d673d7f7701daf819fc
SafiUllah <safiullahkhokhar7@gmail.com>
2023-10-13 22:22:09 +0500

    local v/s global variable

---
+++ lec23LocalvdGlobalvar.py
@@ -0,0 +1,9 @@
+# local v/s global variable
+
+# local variable is a variable that is defined within a function and is only accessible within that functions.
+# It's only created when the function is called and is destroyes when the function returns.
+
+
+
+
+# globle variable that is defined outside of a function and is accessible from within any function in your code.
