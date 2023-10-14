607c3c0536c3c350890e63ad7dfb057adaef6aff
SafiUllah <safiullahkhokhar7@gmail.com>
2023-10-14 23:40:04 +0500

    readline and writelines method

--- lec23FileOS/ReadFile.py
+++ lec23FileOS/ReadFile.py
@@ -1,4 +1,4 @@
-1# lecture1.txt file move outside the lec22FileModule than execute these programes
+# lecture1.txt file move outside the lec22FileModule than execute these programes
 # file input and output
 # read only file
 # Python provides the open() function to open a file. It takes two arguments: the name of the file and the mode in which the file should be opened. 
@@ -28,11 +28,45 @@
 #keyword
 # you can use the 'with' statement to automatically close the file after you are done with it
 
-with open("lecacture1.txt",'a') as f:
-    f.write("hi I am safiullah...")
+# with open("lecture1.txt",'a') as f:
+#     f.write("hi I am safiullah...")
 
 
 # creat a file 
 # This mode creates a file and gives an error if the file already exists.
 
 
+# readline()
+# The readline() method reads a single line from the file. If we want to read multiple lines, we can use a loop.
+
+# f = open('myfile.txt', 'r')
+# i = 0
+# while True:
+#     i = i+1
+#     line = f.readline()
+#     if not line:
+#         break
+#     m1 = int(line.split(",")[0])
+#     m2 = int(line.split(",")[1])
+#     m3 = int(line.split(",")[2])
+#     print(f"Marks of student of {i} in Maths is: {m1 * 2}")
+#     print(f"Marks of student of {i} in English is: {m2 * 2}")
+#     print(f"Marks of student of {i} in Cs is: {m3 * 2}")
+#     print(line)
+
+# writelines()
+# Python writes a sequence of strings to a file. 
+# The sequence can be any iterable object, such as a list or a tuple.
+
+# f = open('myfile2.txt', 'w') # if file is not aviable than create a new file
+# lines = ['line1\n','line2\n','line3\n','line4\n'] # in file print lines 1 to 4
+# f.writelines(lines)
+# f.close()
+
+# If you want to add newlines between the strings, 
+# you can use a loop to write each string separately
+f = open('myfile2.txt', 'w') # if file is not aviable than create a new file
+lines = ['line1\n','line2\n','line3\n','line4\n'] # in file print lines 1 to 4
+for line in lines:
+        f.write(line + '\n')
+f.close()
