bd0a13f58a9ed6d61d0e95911e43bfc225d00146
SafiUllah <safiullahkhokhar7@gmail.com>
2023-10-23 16:20:14 +0500

    Game

--- Exercise5snakewategun.py
+++ Exercise5snakewategun.py
@@ -1,4 +1,4 @@
-# snake water gun
+# snake water gun game
 # Snake beats Water
 # Water beats Gun
 # Gun beats Snake
@@ -7,15 +7,12 @@ import random
 # random numbers select by comp
 # print(random.randint(0 , 2))
 # print(random.randint(0 , 2))
-# print(random.randint(0 , 2))
-# print(random.randint(0 , 2))
-# print(random.randint(0 , 2))
 
-comp = (random.randint(0,2))
-user = int(input("0 for Snake, 1 for Water and 2 for Gun: "))
-
+comp = (random.randint(0,2)) # computer random number
+user = int(input("0 for Snake, 1 for Water and 2 for Gun: ")) # user take any num
+# method for how mush chances that you were lose the game
 def check(comp , user):
-    if comp == user:
+    if comp == user: # draw game 
         return 0
     if (comp == 0 and user ==1):
         return -1
@@ -23,14 +20,14 @@ def check(comp , user):
         return -1
     if (comp == 2 and user ==0):
         return -1
-    return 1
+    return 1 # otherwise you win
 
 score = check(comp,user)
 
 print("You:", user)
-print("Computer:", comp)
+print("Computer:", comp) # which num computer select
 
-if(score ==0):
+if(score == 0):
     print("It's Draw")
 elif (score == -1):
     print("You Lose")
